44a7b8d9e0d867162f01bf11a147068f84ccc5ca
Davidasx <digcboy@163.com>
2024-09-12 14:11:08 +0800

    New

--- wordle.cpp
+++ wordle.cpp
@@ -8,8 +8,8 @@
 #include<windows.h>
 #include<graphics.h>
 using namespace std;
-string ver="3.3.18";
-string date="20240911";
+string ver="3.3.19";
+string date="20240912";
 string config="\\wordle.conf";
 vector<string> keyboard={ "Q W E R T Y U I O P",
 						 " A S D F G H J K L",
